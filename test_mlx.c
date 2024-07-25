8c34abe200bec0303855a57bae8718f94eaaa40c
Reem Essam <60492880+ReemEssamEldin@users.noreply.github.com>
2024-07-25 11:21:38 +0200

    done

--- test_mlx.c
+++ test_mlx.c
@@ -1,24 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_mlx.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: reldahli <reldahli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 14:17:40 by reldahli          #+#    #+#             */
+/*   Updated: 2024/07/24 14:22:49 by reldahli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <mlx.h>
 #include <stdlib.h>
 #include <stdio.h>
-int main()
+
+int	main(void)
 {
-    void *mlx;
-    void *win;
+	void	*mlx;
+	void	*win;
 
-    mlx = mlx_init();
-    if (mlx == NULL)
-    {
-        printf("mlx_init failed\n");
-        return (1); // mlx_init failed
-    }
-
-    win = mlx_new_window(mlx, 800, 600, "Test Window");
-    if (win == NULL)
-    {
-        return (1); // mlx_new_window failed
-    }
-
-    mlx_loop(mlx);
-    return (0);
+	mlx = mlx_init();
+	if (mlx == NULL)
+	{
+		printf("mlx_init failed\n");
+		return (1);
+	}
+	win = mlx_new_window(mlx, 800, 600, "Test Window");
+	if (win == NULL)
+	{
+		return (1);
+	}
+	mlx_loop(mlx);
+	return (0);
 }
